fdde36c79db32a99651fbba2a0bce6e0e643b7d2
Carly Gordon <carlygordon97@gmail.com>
2023-01-10 15:01:54 -0800

    e11

--- part01-e11_interleave/src/interleave.py
+++ part01-e11_interleave/src/interleave.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def interleave(*lists):
-    return []
+    new_list = []
+    x = list(zip(lists))
+
+    for row in range(len(x[0][0])):
+        for col in range(len(x)):
+            new_list.append(x[col][0][row])
+
+    return new_list
 
 def main():
     print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
