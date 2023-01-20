01463fbeb1a96191e0868a8f23361ff0d93322f4
Carly Gordon <carlygordon97@gmail.com>
2023-01-19 16:57:33 -0800

    e14

--- part01-e14_find_matching/src/find_matching.py
+++ part01-e14_find_matching/src/find_matching.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
+# Write function find_matching that gets a list of strings and a search string
+# as parameters. The function should return the indices to those elements in the
+ # input list that contain the search string. Use the function enumerate.
 
 def find_matching(L, pattern):
-    return []
+    idx_match = []
+    for i, wrd in enumerate(L):
+        if pattern in wrd:
+            idx_match.append(i)
+
+    return idx_match
 
 def main():
-    pass
+    assert (
+        find_matching(["sensitive", "engine", "rubbish", "comment"], "en") == [0, 1, 3]
+    )
 
 if __name__ == "__main__":
     main()
