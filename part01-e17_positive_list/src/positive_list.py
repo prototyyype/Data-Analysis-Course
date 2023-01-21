e6f2650e193f97c5efbb28f6d1e69adb448036c1
Carly Gordon <carlygordon97@gmail.com>
2023-01-20 17:11:18 -0800

    e17

--- part01-e17_positive_list/src/positive_list.py
+++ part01-e17_positive_list/src/positive_list.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
+'''
+Write a function positive_list that gets a list of numbers as a parameter, and
+returns a list with the negative numbers and zero filtered out using the filter
+function.
 
+The function call positive_list([2,-2,0,1,-7]) should return the list [2,1].
+Test your function in the main function.
+'''
 def positive_list(L):
-    return []
+    return list(filter(lambda x: x > 0, L))
 
 def main():
-    pass
+    assert positive_list([2,-2,0,1,-7]) == [2,1]
 
 if __name__ == "__main__":
     main()
