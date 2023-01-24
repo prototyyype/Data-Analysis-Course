cd77de7f6e58183c9b16a74ac71ec98a0732988a
Carly Gordon <carlygordon97@gmail.com>
2023-01-23 18:26:19 -0800

    e20

--- part01-e20_usemodule/src/triangle.py
+++ part01-e20_usemodule/src/triangle.py
@@ -1,1 +1,16 @@
 # Enter you module contents here
+"""The module contains two functions: hypotenuse and area."""
+import math
+
+__version__ = "1.3"
+__author__ = "prototyyype"
+
+def hypotenuse(a, b):
+    """the hypotenuse function returns the length of the hypotenuse when given
+    the lengths of two other sides of a right-angled triangle"""
+    return math.sqrt(a*a + b*b)
+
+def area(a, b):
+    """the area function returns the area of the right-angled triangle, when two sides,
+    perpendicular to each other, are given as parameters."""
+    return 0.5*a*b
--- part01-e20_usemodule/src/usemodule.py
+++ part01-e20_usemodule/src/usemodule.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+'''
+Create your own module as file triangle.py in the src folder. The module should
+contain two functions:
+
+* hypotenuse: which returns the length of the hypotenuse when given the lengths of
+two other sides of a right-angled triangle
+
+* area: which returns the area of the
+right-angled triangle, when two sides, perpendicular to each other, are given
+as parameters.
+
+Make sure both the functions and the module have descriptive
+docstrings. Add also the __version__ and __author__ attributes to the module.
+Call both your functions from the main function (which is in file usemodule.py).
+'''
 
 # Don't modify the below hack
 try:
@@ -8,6 +23,9 @@ except ModuleNotFoundError:
 
 def main():
     # Call the functions from here
+    triangle.hypotenuse(3, 4) #== 5
+    triangle.area(3,4) #== 6
+
 
 if __name__ == "__main__":
     main()
